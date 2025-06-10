a14b4b375e52a30ed5b89f9d698689bc176d8ec5
Vrynd <vryndhika@gmail.com>
2025-06-11 06:37:08 +0700

    Perubahan Front-end

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,22 +24,23 @@
             </a>
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                 <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" type="button"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg md:gap-3 text-sm px-5 py-2.5 text-center inline-flex items-center ">Masuk
+                    class="text-white gap-3 bg-blue-700 hover:bg-blue-800 md:focus:ring-4 md:focus:outline-none md:focus:ring-blue-300 font-medium rounded-lg md:gap-3 text-sm px-5 py-2.5 text-center inline-flex items-center ">Masuk
                     Sebagai
                     <i class="ri-arrow-drop-down-line text-base"></i>
                 </button>
             </div>
             <!-- Dropdown menu -->
-            <div id="dropdown"
-                class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700">
-                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+            <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44">
+                <ul class="py-2 text-sm " aria-labelledby="dropdownDefaultButton">
                     <li>
                         <a href="/login"
-                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Pencari Kos</a>
+                            class="block px-4 py-2 hover:bg-gray-100  text-gray-700 hover:text-blue-500">Pencari
+                            Kos</a>
                     </li>
                     <li>
                         <a href="admin/login"
-                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Pemilik Kos
+                            class="block px-4 py-2 hover:bg-gray-100  text-gray-700 hover:text-blue-500">Pemilik
+                            Kos
                         </a>
                     </li>
                 </ul>
@@ -52,22 +53,17 @@
                 <h1
                     class=" mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
                     Selamat Datang di Sistem Reservasi Kos</h1>
-                <p class=" mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From
+                <p class=" mb-6 mt-4 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From
                     checkout to global sales tax compliance, companies around the world use Flowbite to simplify their
                     payment stack.</p>
-                <a href="#"
-                    class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                    Get started
-                    <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                            clip-rule="evenodd"></path>
-                    </svg>
+                <a href="/login"
+                    class="inline-flex gap-3 md:gap-5items-center justify-center px-5 py-3 mr-3 text-lg font-medium text-center text-white rounded-lg bg-primary-700 ">
+                    Masuk
+                    <i class="ri-arrow-right-line text-lg"></i>
                 </a>
-                <a href="#"
-                    class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                    Speak to Sales
+                <a href="#features"
+                    class="inline-flex items-center justify-center px-5 py-3 text-lg font-medium text-center text-black border border-gray-300 rounded-lg hover:bg-blue-400 hover:text-white ">Lihat
+                    Fitur
                 </a>
             </div>
             <div class=" lg:mt-0 lg:col-span-5 lg:flex">
@@ -76,66 +72,101 @@
         </div>
     </section>
 
-
-
-
-
-    <!-- Hero Section -->
-    {{-- <section class="hero">
-        <div class="container hero-container">
-            <div class="hero-content" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000">
-                <h1>Selamat Datang Sistem Pencarian Kos</h1>
-                <h2>Temukan Kos Impian Anda</h2>
-                <p>Platform terbaik untuk mencari kos dengan fasilitas lengkap, lokasi strategis yang dekat dengan pusat
-                    aktivitas, serta harga terjangkau sesuai dengan kebutuhan mahasiswa atau pekerja.</p>
-                <a href="#features" class="cta-btn" data-aos="fade-right" data-aos-delay="100"
-                    data-aos-duration="1000">Apa Yang Ditawarkan?</a>
-                <a href="/login" class="cta-btn" data-aos="fade-left" data-aos-delay="100"
-                    data-aos-duration="1000">Coba Sekarang</a>
+    <section class="bg-gray-100" id="features">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+            <div class="max-w-screen-md mb-8 lg:mb-16">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Apa yang Kami
+                    Tawarkan?</h2>
             </div>
-            <div class="hero-image" data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">
-                <img src="/images/forSaleIcon.png" alt="Ilustrasi Kos" />
-            </div>
-        </div>
-    </section> --}}
-
-    <!-- Features Section -->
-    <section class="features" id="features">
-        <div class="container">
-            <h2>Apa yang Kami Tawarkan?</h2>
-            <div class="features-grid" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000">
-                <div class="feature-item" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000">
-                    <img src="/images/vektor/option.png" alt="Beragam Pilihan">
-                    <h3>Beragam Pilihan</h3>
-                    <p>Temukan kos dengan berbagai fasilitas sesuai kebutuhan Anda.</p>
+            <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+                <div>
+                    <div
+                        class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                        <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor"
+                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold dark:text-white">Beragam Pilihan</h3>
+                    <p class="text-gray-500 dark:text-gray-400 mt-3">Temukan kos dengan berbagai fasilitas sesuai kebutuhan Anda.</p>
                 </div>
-                <div class="feature-item" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000">
-                    <img src="/images/vektor/Search.png" alt="Beragam Pilihan">
-                    <h3>Fitur Pencarian yang Mudah</h3>
-                    <p>Filter berdasarkan lokasi, harga, fasilitas, tipe kos.</p>
+                <div>
+                    <div
+                        class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                        <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor"
+                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z">
+                            </path>
+                        </svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold dark:text-white">Fitur Pencarian yang Mudah</h3>
+                    <p class="text-gray-500 dark:text-gray-400 mt-3">Filter berdasarkan lokasi, harga, fasilitas, tipe kos.</p>
                 </div>
-                <div class="feature-item" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000">
-                    <img src="/images/vektor/Contact.png" alt="Beragam Pilihan">
-                    <h3>Informasi yang Detail</h3>
-                    <p>Foto kos yang lengkap dan Deskripsi fasilitas (kamar mandi dalam, AC, Wi-Fi, dapur).</p>
+                <div>
+                    <div
+                        class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                        <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor"
+                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
+                                clip-rule="evenodd"></path>
+                            <path
+                                d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z">
+                            </path>
+                        </svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold dark:text-white">Informasi yang Detail</h3>
+                    <p class="text-gray-500 dark:text-gray-400 mt-3">Foto kos yang lengkap dan Deskripsi fasilitas (kamar mandi dalam, AC, Wi-Fi, dapur).</p>
                 </div>
-                <div class="feature-item" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000">
-                    <img src="/images/vektor/Security.png" alt="Beragam Pilihan">
-                    <h3>Keamanan dan Privasi</h3>
-                    <p>Verifikasi identitas penyewa dan pemilik kos.</p>
+                <div>
+                    <div
+                        class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                        <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor"
+                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z">
+                            </path>
+                            <path fill-rule="evenodd"
+                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold dark:text-white">Keamanan dan Privasi</h3>
+                    <p class="text-gray-500 dark:text-gray-400 mt-3">Kami menggunakan teknologi terkini untuk menjaga keamanan dan privasi Anda.</p>
                 </div>
-                <div class="feature-item" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000">
-                    <img src="/images/vektor/Service.png" alt="Beragam Pilihan">
-                    <h3>Layanan Pelanggan 24/7</h3>
-                    <p>Dukungan melalui fitur kontak dapat diakses dengan mudah dan gampang.</p>
+                <div>
+                    <div
+                        class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                        <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor"
+                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z">
+                            </path>
+                        </svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold dark:text-white">Layanan Pelanggan 24/7</h3>
+                    <p class="text-gray-500 dark:text-gray-400 mt-3">Dukungan melalui fitur kontak dapat diakses dengan mudah dan gampang</p>
+                </div>
+                <div>
+                    <div
+                        class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                        <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor"
+                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                    <h3 class="mb-2 text-xl font-bold dark:text-white">Reservasi Instan</h3>
+                    <p class="text-gray-500 dark:text-gray-400 mt-3">Langsung pesan kamar kos tanpa perlu konfirmasi manual berulang.</p>
                 </div>
             </div>
         </div>
     </section>
-
-
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
-
     <script>
         AOS.init();
     </script>
