ac589cc555e5d308d76db9b4b9d2813478c5cf01
Vrynd <vryndhika@gmail.com>
2025-06-10 21:58:51 +0700

    Membuat Frontend user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,31 +12,56 @@
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
 </head>
-
 <body>
     <!-- Navbar -->
-
-
-    <nav
-        class="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200 shadow-lg">
+    <nav class="bg-white sticky w-full z-20 top-0 start-0 border-b border-gray-200 shadow-lg">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-5">
             <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
                 <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo">
                 <span class="self-center text-2xl font-semibold whitespace-nowrap text-blue-500">Omahku</span>
             </a>
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <a href="{{}}" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none cursor-pointer font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Mulai Sekarang</a>
+                <a href="{{}}"
+                    class="tetx-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none cursor-pointer font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Mulai
+                    Sekarang</a>
             </div>
-
         </div>
     </nav>
+    <section class="bg-white">
+        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+            <div class="mr-auto place-self-center lg:col-span-7">
+                <h1
+                    class=" mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Selamat Datang di Sistem Reservasi Kos</h1>
+                <p class=" mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From
+                    checkout to global sales tax compliance, companies around the world use Flowbite to simplify their
+                    payment stack.</p>
+                <a href="#"
+                    class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                    Get started
+                    <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </a>
+                <a href="#"
+                    class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                    Speak to Sales
+                </a>
+            </div>
+            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+                <img src="images/hero_image.png" alt="mockup">
+            </div>
+        </div>
+    </section>
 
 
 
 
 
     <!-- Hero Section -->
-    <section class="hero">
+    {{-- <section class="hero">
         <div class="container hero-container">
             <div class="hero-content" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000">
                 <h1>Selamat Datang Sistem Pencarian Kos</h1>
@@ -52,7 +77,7 @@
                 <img src="/images/forSaleIcon.png" alt="Ilustrasi Kos" />
             </div>
         </div>
-    </section>
+    </section> --}}
 
     <!-- Features Section -->
     <section class="features" id="features">
